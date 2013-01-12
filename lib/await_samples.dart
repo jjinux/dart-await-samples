b29400708227c66de17e6d8796db2219d47645e8
Shannon -jj Behrens <jjinux@gmail.com>
2013-01-11 19:37:48 -0800

    Showed how to transform Future-based exceptions into exceptions that work with try / catch

--- lib/await_samples.dart
+++ lib/await_samples.dart
@@ -11,25 +11,43 @@ library await_samples;
 import "dart:isolate";
 
 /// This is taken directly from the spec.
-class Bookmark {
+class Bookmark<ReturnType> {
   Set<String> locations;
   Map variables;
+  Completer<ReturnType> completer;
 
   Bookmark() {
     locations = new Set<String>();
     variables = {};
+    assert(completer == null);
   }
-
+  
   Bookmark.from(Bookmark other) {
     locations = new Set<String>.from(other.locations);
     variables = new Map.from(other.variables);
+    completer = other.completer;
   }
 }
 
-/// This is just an example asynchronous function that I can use for testing.
-Future<int> incrementSlowly(int i) {
+/// Increment [i] and wrap it in a future.
+Future<int> asyncIncrement(int i) {
   var completer = new Completer<int>();
   new Timer(0, (t) => completer.complete(i + 1));
+  return completer.future;
+}
+
+/// This is just some random exception class I can use for testing.
+class SomeException implements Exception {
+  String message;
+  SomeException([this.message]);
+}
+
+/// Use [Future.handleError] to "throw" the exception asynchronously. 
+Future asyncThrow(Exception e) {
+  var completer = new Completer();
+  new Timer(0, (t) {
+    completer.completeException(e);
+  }); 
   return completer.future;
 }
 
@@ -43,10 +61,25 @@ Future<int> incrementSlowly(int i) {
 Future<int> wrapWithFuture(int value) {
 
   Future<int> _wrapWithFuture(Bookmark __prevBookmark, int value) {
-    Completer<int> __completer = new Completer<int>();
-    Bookmark __nextBookmark = new Bookmark();
-    __completer.complete(value);
-    return __completer.future;
+    Bookmark __nextBookmark = new Bookmark<int>();    
+    if (__prevBookmark == null) {
+      __nextBookmark.completer = new Completer<int>();
+    } else {
+      __nextBookmark.completer = __prevBookmark.completer;
+    }    
+    Completer<int> __completer = __nextBookmark.completer;
+    if (__prevBookmark == null) {
+      __nextBookmark.variables["value"] = value;
+    }
+    try {
+
+      __completer.complete(value);
+      return __completer.future;
+    
+    } catch (__e, __s) {
+      __completer.completeException(__e, __s);
+      return __completer.future;
+    }
   }
 
   return _wrapWithFuture(null, value);
@@ -64,33 +97,59 @@ Future<int> wrapWithFuture(int value) {
 Future<int> simpleStatement(int value) {
 
   Future<int> _simpleStatement(Bookmark __prevBookmark, int value) {
-    Completer<int> __completer = new Completer<int>();
-    Bookmark __nextBookmark = new Bookmark();
-    __nextBookmark.variables["value"] = value;
-
+    Bookmark __nextBookmark = new Bookmark<int>();    
     if (__prevBookmark == null) {
-      value++;
+      __nextBookmark.completer = new Completer<int>();
+    } else {
+      __nextBookmark.completer = __prevBookmark.completer;
+    }    
+    Completer<int> __completer = __nextBookmark.completer;
+    if (__prevBookmark == null) {
+      __nextBookmark.variables["value"] = value;
     }
-    
-    if (__prevBookmark == null) {
-      incrementSlowly(value).then((__value) {
-        __nextBookmark.locations.add("await1");
-        __nextBookmark.variables["value"] = __value;
-        Bookmark __copy = new Bookmark.from(__nextBookmark);
-        _simpleStatement(__copy, value).then((__value) {
-          __completer.complete(__value);
+    try {
+
+      if (__prevBookmark == null) {
+        value++;
+      }
+      
+      if (__prevBookmark == null) {
+        Future<int> __asyncCallFuture = asyncIncrement(value);
+        
+        __reEnterFunction() {
+          __nextBookmark.locations.add("await1");
+          Bookmark __copy = new Bookmark.from(__nextBookmark);
+          _simpleStatement(__copy, value);            
+        }
+        
+        __asyncCallFuture.then((__value) {
+          __nextBookmark.variables["value"] = __value;            
+          __reEnterFunction();
         });
-      });
+        
+        __asyncCallFuture.handleException((__e) {
+          __nextBookmark.variables["__exceptionToThrow"] = __e;            
+          __reEnterFunction();
+          return true;
+        });
+        
+        return __completer.future;
+      }
+      
+      if (__prevBookmark.locations.contains("await1")) {
+        value = __nextBookmark.variables["value"] = __prevBookmark.variables["value"];
+        var __exceptionToThrow = __prevBookmark.variables["__exceptionToThrow"];
+        __prevBookmark = null;
+        if (__exceptionToThrow != null) throw __exceptionToThrow; 
+      }
+      
+      __completer.complete(value);
+      return __completer.future;
+      
+    } catch (__e, __s) {
+      __completer.completeException(__e, __s);
       return __completer.future;
     }
-    
-    if (__prevBookmark.locations.contains("await1")) {
-      value = __nextBookmark.variables["value"] = __prevBookmark.variables["value"];
-      __prevBookmark = null;
-    }
-    
-    __completer.complete(value);
-    return __completer.future;
   }
 
   return _simpleStatement(null, value);
@@ -112,53 +171,79 @@ Future<int> simpleStatement(int value) {
 Future<int> whileLoop(int value) {
 
   Future<int> _whileLoop(Bookmark __prevBookmark, int value) {
-    Completer<int> __completer = new Completer<int>();
-    Bookmark __nextBookmark = new Bookmark();
+    Bookmark __nextBookmark = new Bookmark<int>();    
+    if (__prevBookmark == null) {
+      __nextBookmark.completer = new Completer<int>();
+    } else {
+      __nextBookmark.completer = __prevBookmark.completer;
+    }    
+    Completer<int> __completer = __nextBookmark.completer;
     if (__prevBookmark == null) {
       __nextBookmark.variables["value"] = value;
     }
-    
-    int sum;
-    if (__prevBookmark == null) {
-      sum = __nextBookmark.variables["sum"] = 0;
-    }
-    
-    if (__prevBookmark == null ||
-        __prevBookmark.locations.contains("while1")) {
-      
-      __nextBookmark.locations.add("while1");
-      while ((__prevBookmark == null &&
-              value > 0) ||
-             (__prevBookmark != null &&
-              __prevBookmark.locations.remove("while1"))) {
+    try {
 
-        int returnedValue;
-        if (__prevBookmark == null) {
-          wrapWithFuture(value).then((__value) {
-            __nextBookmark.locations.add("await1");
-            __nextBookmark.variables["returnedValue"] = __value;
-            Bookmark __copy = new Bookmark.from(__nextBookmark);
-            _whileLoop(__copy, value).then((__value) {
-              __completer.complete(__value);
-            });
-          });
-          return __completer.future;
-        }
-        if (__prevBookmark.locations.contains("await1")) {
-          returnedValue = __nextBookmark.variables["returnedValue"] = __prevBookmark.variables["returnedValue"];
-          value = __nextBookmark.variables["value"] = __prevBookmark.variables["value"];
-          sum = __nextBookmark.variables["sum"] = __prevBookmark.variables["sum"];
-          __prevBookmark = null;
-        }        
-        
-        sum = __nextBookmark.variables["sum"] = sum + returnedValue;
-        value = __nextBookmark.variables["value"] = value - 1;        
+      int sum;
+      if (__prevBookmark == null) {
+        sum = __nextBookmark.variables["sum"] = 0;
       }
-      __nextBookmark.locations.remove("while1");
+      
+      if (__prevBookmark == null ||
+          __prevBookmark.locations.contains("while1")) {
+        
+        __nextBookmark.locations.add("while1");
+        while ((__prevBookmark == null &&
+                value > 0) ||
+               (__prevBookmark != null &&
+                __prevBookmark.locations.remove("while1"))) {
+  
+
+          var returnedValue;
+          if (__prevBookmark == null) {
+            Future<int> __asyncCallFuture = wrapWithFuture(value);
+            
+            __reEnterFunction() {
+              __nextBookmark.locations.add("await1");
+              Bookmark __copy = new Bookmark.from(__nextBookmark);
+              _whileLoop(__copy, value);            
+            }
+            
+            __asyncCallFuture.then((__value) {
+              __nextBookmark.variables["returnedValue"] = __value;            
+              __reEnterFunction();
+            });
+            
+            __asyncCallFuture.handleException((__e) {
+              __nextBookmark.variables["__exceptionToThrow"] = __e;            
+              __reEnterFunction();
+              return true;
+            });
+            
+            return __completer.future;
+          }
+          
+          if (__prevBookmark.locations.contains("await1")) {
+            returnedValue = __nextBookmark.variables["returnedValue"] = __prevBookmark.variables["returnedValue"];
+            var __exceptionToThrow = __prevBookmark.variables["__exceptionToThrow"];
+            value = __nextBookmark.variables["value"] = __prevBookmark.variables["value"];
+            sum = __nextBookmark.variables["sum"] = __prevBookmark.variables["sum"];
+            __prevBookmark = null;
+            if (__exceptionToThrow != null) throw __exceptionToThrow; 
+          }
+          
+          sum = __nextBookmark.variables["sum"] = sum + returnedValue;
+          value = __nextBookmark.variables["value"] = value - 1;        
+        }
+        __nextBookmark.locations.remove("while1");
+      }
+      
+      __completer.complete(sum);
+      return __completer.future;
+
+    } catch (__e, __s) {
+      __completer.completeException(__e, __s);
+      return __completer.future;
     }
-    
-    __completer.complete(sum);
-    return __completer.future;
   }
 
   return _whileLoop(null, value);
@@ -178,45 +263,169 @@ Future<int> whileLoop(int value) {
 Future<int> recursivelySum(int value) {
 
   Future<int> _recursivelySum(Bookmark __prevBookmark, int value) {
-    Completer<int> __completer = new Completer<int>();
-    Bookmark __nextBookmark = new Bookmark();
-
+    Bookmark __nextBookmark = new Bookmark<int>();    
+    if (__prevBookmark == null) {
+      __nextBookmark.completer = new Completer<int>();
+    } else {
+      __nextBookmark.completer = __prevBookmark.completer;
+    }    
+    Completer<int> __completer = __nextBookmark.completer;
     if (__prevBookmark == null) {
       __nextBookmark.variables["value"] = value;
     }
+    try {
 
-    if ((__prevBookmark == null && value <= 0) ||
-        (__prevBookmark != null &&
-         __prevBookmark.locations.contains("if1"))) {
-      __nextBookmark.locations.add("if1");
+      if ((__prevBookmark == null && value <= 0) ||
+          (__prevBookmark != null &&
+           __prevBookmark.locations.contains("if1"))) {
+        __nextBookmark.locations.add("if1");
+        
+        __completer.complete(value);
+        return __completer.future;
+  
+        __nextBookmark.locations.remove("if1");
+      }
       
-      __completer.complete(value);
-      return __completer.future;
-
-      __nextBookmark.locations.remove("if1");
-    }
-    
-    var sum;
-    if (__prevBookmark == null) {
-      recursivelySum(value - 1).then((__value) {
-        __nextBookmark.locations.add("await1");
-        __nextBookmark.variables["sum"] = __value;
-        Bookmark __copy = new Bookmark.from(__nextBookmark);
-        _recursivelySum(__copy, value).then((__value) {
-          __completer.complete(__value);
+      var sum;
+      if (__prevBookmark == null) {
+        Future<int> __asyncCallFuture = recursivelySum(value - 1);
+        
+        __reEnterFunction() {
+          __nextBookmark.locations.add("await1");
+          Bookmark __copy = new Bookmark.from(__nextBookmark);
+          _recursivelySum(__copy, value);            
+        }
+        
+        __asyncCallFuture.then((__value) {
+          __nextBookmark.variables["sum"] = __value;            
+          __reEnterFunction();
         });
-      });
+        
+        __asyncCallFuture.handleException((__e) {
+          __nextBookmark.variables["__exceptionToThrow"] = __e;            
+          __reEnterFunction();
+          return true;
+        });
+        
+        return __completer.future;
+      }
+      
+      if (__prevBookmark.locations.contains("await1")) {
+        sum = __nextBookmark.variables["sum"] = __prevBookmark.variables["sum"];
+        var __exceptionToThrow = __prevBookmark.variables["__exceptionToThrow"];
+        value = __nextBookmark.variables["value"] = __prevBookmark.variables["value"];
+        __prevBookmark = null;
+        if (__exceptionToThrow != null) throw __exceptionToThrow; 
+      }
+      
+      __completer.complete(sum + value);
       return __completer.future;
-    }
-    if (__prevBookmark.locations.contains("await1")) {
-      sum = __nextBookmark.variables["sum"] = __prevBookmark.variables["sum"];
-      value = __nextBookmark.variables["value"] = __prevBookmark.variables["value"];
-      __prevBookmark = null;
-    }
-    
-    __completer.complete(sum + value);
-    return __completer.future;
+      
+    } catch (__e, __s) {
+      __completer.completeException(__e, __s);
+      return __completer.future;
+    }    
   }
 
   return _recursivelySum(null, value);
 }
+
+/**
+ * Test throw.
+ * 
+ * async throwInMultipleWays(int value) {
+ *   if (value == 0) throw new SomeException("Throw before <-");
+ *   if (value == 1) {
+ *     <- asyncThrow(new SomeException("Throw during <-"));
+ *   }
+ *   throw new SomeException("Throw after <-");
+ * }
+ * 
+ * All three of these exceptions should be transformed into
+ * [__completer.completeException].
+ **/
+Future throwInMultipleWays(int value) {
+  Future _throwInMultipleWays(Bookmark __prevBookmark, value) {
+    Bookmark __nextBookmark = new Bookmark<int>();    
+    if (__prevBookmark == null) {
+      __nextBookmark.completer = new Completer<int>();
+    } else {
+      __nextBookmark.completer = __prevBookmark.completer;
+    }    
+    Completer<int> __completer = __nextBookmark.completer;
+    if (__prevBookmark == null) {
+      __nextBookmark.variables["value"] = value;
+    }
+    try {
+      
+      if ((__prevBookmark == null && value == 0) ||
+          (__prevBookmark != null &&
+           __prevBookmark.locations.contains("if0"))) {
+        __nextBookmark.locations.add("if0");
+        
+        // Throw exception for value 0
+        throw new SomeException("Throw before <-");
+        
+        __nextBookmark.locations.remove("if0");
+      }
+      
+      if ((__prevBookmark == null && value == 1) ||
+          (__prevBookmark != null &&
+           __prevBookmark.locations.contains("if1"))) {
+        __nextBookmark.locations.add("if1");
+  
+        var __ignored1;
+        if (__prevBookmark == null) {
+          
+          // Async throw for value 1
+          Future __asyncCallFuture = asyncThrow(new SomeException("Throw during <-"));
+          
+          __reEnterFunction() {
+            __nextBookmark.locations.add("await1");
+            Bookmark __copy = new Bookmark.from(__nextBookmark);
+            _throwInMultipleWays(__copy, value);            
+          }
+          
+          __asyncCallFuture.then((__value) {
+            __nextBookmark.variables["__ignored1"] = __value;            
+            __reEnterFunction();
+          });
+          
+          // handleException for value 1 by re-entering the function
+          // and throwing it there.
+          __asyncCallFuture.handleException((__e) {
+            __nextBookmark.variables["__exceptionToThrow"] = __e;            
+            __reEnterFunction();
+            return true;
+          });
+          
+          return __completer.future;
+        }
+        
+        if (__prevBookmark.locations.contains("await1")) {
+          __ignored1 = __nextBookmark.variables["__ignored1"] = __prevBookmark.variables["__ignored1"];
+          var __exceptionToThrow = __prevBookmark.variables["__exceptionToThrow"];
+          value = __nextBookmark.variables["value"] = __prevBookmark.variables["value"];
+          __prevBookmark = null;
+          if (__exceptionToThrow != null) throw __exceptionToThrow; 
+        }
+  
+        __nextBookmark.locations.remove("if1");
+      }
+  
+      // Throw exception for value 2
+      throw new SomeException("Throw after <-");
+
+      __completer.complete(null);
+      return __completer.future;
+      
+    // All 3 unhandled exceptions should propagate to here. 
+    } catch (__e, __s) {
+      __completer.completeException(__e, __s);
+      return __completer.future;
+    }    
+  }
+
+  return _throwInMultipleWays(null, value);
+}
+
--- test/await_samples_test.dart
+++ test/await_samples_test.dart
@@ -3,11 +3,19 @@ import 'package:dart_await_samples/await_samples.dart';
 
 void main() {
   group('await_samples', () {
-    test("incrementSlowly", () {
+    test("asyncIncrement", () {
       var checkResult = expectAsync1((value) => expect(value, 1));
-      incrementSlowly(0).then(checkResult);
+      asyncIncrement(0).then(checkResult);
     });
     
+    test("asyncThrow", () {
+      var handleException = expectAsync1((e) {
+        assert(e is SomeException);
+        expect(e.message, "Expected");
+      });
+      asyncThrow(new SomeException("Expected")).handleException(handleException);
+    });
+ 
     test("wrapWithFuture", () {
       var checkResult = expectAsync1((value) => expect(value, 0));
       wrapWithFuture(0).then(checkResult);
@@ -26,6 +34,32 @@ void main() {
     test("recursivelySum", () {
       var checkResult = expectAsync1((value) => expect(value, 3 + 2 + 1));
       recursivelySum(3).then(checkResult);
+    });
+    
+    group('throwInMultipleWays', () {
+      test("Handles exceptions before <-", () {
+        var handleException = expectAsync1((e) {
+          assert(e is SomeException);
+          expect(e.message, "Throw before <-");
+        });
+        throwInMultipleWays(0).handleException(handleException);
+      });
+
+      test("Handles exceptions in function called with <-", () {
+        var handleException = expectAsync1((e) {
+          assert(e is SomeException);
+          expect(e.message, "Throw during <-");
+        });
+        throwInMultipleWays(1).handleException(handleException);
+      });
+
+      test("Handles exceptions after <-", () {
+        var handleException = expectAsync1((e) {
+          assert(e is SomeException);
+          expect(e.message, "Throw after <-");
+        });
+        throwInMultipleWays(2).handleException(handleException);
+      });
     });
   });
 }
